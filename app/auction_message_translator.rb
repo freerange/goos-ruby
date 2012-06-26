8b8cffa85d89d03e05eb603ca5258976390a88c8
James Mead <james@floehopper.org>
2012-06-26 10:39:03 +0100

    "Tidying Up the Translator" in "Sending a Bid" in chapter 13.
    - It's not possible to have *private* inner class in Ruby.
    - I've continued to use the explicit methods on Hash i.e. `Hash#fetch`
      and `Hash#store` to keep the code closer to the Java equivalent.
      However the whole AuctionEvent class feels quite un-Ruby-ish.

--- app/auction_message_translator.rb
+++ app/auction_message_translator.rb
@@ -1,29 +1,61 @@
 class AuctionMessageTranslator
+
+  class AuctionEvent
+    def initialize
+      @fields = Hash.new
+    end
+
+    def type
+      return get("Event")
+    end
+
+    def current_price
+      return get_int("CurrentPrice")
+    end
+
+    def increment
+      return get_int("Increment")
+    end
+
+    def get_int(field_name)
+      return Integer(get(field_name))
+    end
+
+    def get(field_name)
+      return @fields.fetch(field_name)
+    end
+
+    def add_field(field)
+      pair = field.split(":")
+      @fields.store(pair[0].strip, pair[1].strip)
+    end
+
+    class << self
+      def from(message_body)
+        event = AuctionEvent.new
+        fields_in(message_body).each do |field|
+          event.add_field(field)
+        end
+        return event
+      end
+
+      def fields_in(message_body)
+        message_body.split(";")
+      end
+    end
+  end
+
   def initialize(listener)
     @listener = listener
   end
 
   def processMessage(chat, message)
-    event = unpack_event_from(message)
-    type = event.fetch("Event")
-    if "CLOSE" == type
+    event = AuctionEvent.from(message.getBody)
+    event_type = event.type
+    if "CLOSE" == event_type
       @listener.auction_closed
-    elsif "PRICE" == type
-      @listener.current_price(
-        Integer(event.fetch("CurrentPrice")),
-        Integer(event.fetch("Increment"))
-      )
+    elsif "PRICE" == event_type
+      @listener.current_price(event.current_price, event.increment)
     end
-  end
-
-  private
-
-  def unpack_event_from(message)
-    event = Hash.new
-    message.getBody.split(";").each do |element|
-      pair = element.split(":")
-      event.store(pair[0].strip, pair[1].strip)
-    end
-    return event
   end
 end

9a458862ed82df2a98f35c7fe8ff1c89edc90b68
James Mead <james@floehopper.org>
2013-08-17 11:03:25 +0100

    "Follow Through - Object-Oriented Column" in chapter 15.
    > The next change is to replace the switch statement which is noisy, not very
    > object-oriented, and includes an unnecessary default: clause just to satisfy
    > the compiler. It’s served its purpose, which was to get us through the
    > previous coding stage. We add a method to `Column` that will extract the
    > appropriate field.
    >
    > Of course, we write a unit test for `Column`. It may seem unnecessary now,
    > but it will protect us when we make changes and forget to keep the column
    > mapping up to date.

--- app/ui/column.rb
+++ app/ui/column.rb
@@ -1,9 +1,27 @@
+require "ui/main_window"
+
 class Column < Struct.new(:name)
 
   ITEM_IDENTIFIER = new("ITEM_IDENTIFIER")
   LAST_PRICE = new("LAST_PRICE")
   LAST_BID = new("LAST_BID")
   SNIPER_STATE = new("SNIPER_STATE")
+
+  def ITEM_IDENTIFIER.value_in(snapshot)
+    snapshot.item_id
+  end
+
+  def LAST_PRICE.value_in(snapshot)
+    snapshot.last_price
+  end
+
+  def LAST_BID.value_in(snapshot)
+    snapshot.last_bid
+  end
+
+  def SNIPER_STATE.value_in(snapshot)
+    MainWindow::SnipersTableModel.text_for(snapshot.state)
+  end
 
   def ordinal
     return self.class.values.index(self)

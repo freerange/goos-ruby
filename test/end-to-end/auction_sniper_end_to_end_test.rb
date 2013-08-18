678174ebe42118726478b01f5755fad6653b997f
James Mead <james@floehopper.org>
2013-08-18 12:59:54 +0100

    "Testing for Multiple Items - The Application Runner" in ch 16.
    > The rest is similar, which means we can write a new test.
    >
    > Following the protocol convention, we also remember to add a new user,
    > `auction-item-65432`, to the chat server to represent the new auction.
    >
    > The one significant change we have to make in the `ApplicationRunner` is to
    > the `#start_bidding_in` method. Now it needs to accept a variable number of
    > auctions passed through to the Sniper’s command line. The conversion is a
    > bit messy since we have to unpack the item identifiers and append them to the
    > end of the other command-line arguments — this is the best we can do with
    > Java arrays.
    >
    > We run the test and watch it fail:
    
          1) Error:
        test_0004_bids_for_multiple_items(AuctionsniperSpec):
        NativeException: java.lang.AssertionError: Message
        Expected: is not null
             but: was null
    
    Notes:
    
    * Using the Ruby splat operator and `Array#map` I've avoided the need for
      the `#arguments` method mentioned in the book.
    * In order to see the test fail in the way described in the book, I found that
      I needed to postpone to a later commit the change to
      `ApplicationRunner#start_bidding_in` which checks the sniper status in the UI
      for *each* auction.

--- test/end-to-end/auction_sniper_end_to_end_test.rb
+++ test/end-to-end/auction_sniper_end_to_end_test.rb
@@ -7,11 +7,13 @@ require "end-to-end/application_runner"
 describe AuctionSniper do
   before do
     @auction = FakeAuctionServer.new("item-54321")
+    @auction2 = FakeAuctionServer.new("item-65432")
     @application = ApplicationRunner.new
   end
 
   after do
     @auction.stop
+    @auction2.stop
     @application.stop
   end
 
@@ -45,5 +47,25 @@ describe AuctionSniper do
     @application.has_shown_sniper_is_winning(@auction, 1098)
     @auction.announce_closed
     @application.shows_sniper_has_won_auction(@auction, 1098)
+  end
+
+  it "bids for multiple items" do
+    @auction.start_selling_item
+    @auction2.start_selling_item
+    @application.start_bidding_in(@auction, @auction2)
+    @auction.has_received_join_request_from(ApplicationRunner::SNIPER_XMPP_ID)
+    @auction2.has_received_join_request_from(ApplicationRunner::SNIPER_XMPP_ID)
+    @auction.report_price(1000, 98, "other bidder")
+    @auction.has_received_bid(1098, ApplicationRunner::SNIPER_XMPP_ID)
+    @auction2.report_price(500, 21, "other bidder")
+    @auction2.has_received_bid(521, ApplicationRunner::SNIPER_XMPP_ID)
+    @auction.report_price(1098, 97, ApplicationRunner::SNIPER_XMPP_ID)
+    @auction2.report_price(521, 22, ApplicationRunner::SNIPER_XMPP_ID)
+    @application.has_shown_sniper_is_winning(@auction, 1098)
+    @application.has_shown_sniper_is_winning(@auction2, 521)
+    @auction.announce_closed
+    @auction2.announce_closed
+    @application.shows_sniper_has_won_auction(@auction, 1098)
+    @application.shows_sniper_has_won_auction(@auction2, 521)
   end
 end

b942c2fabeb9c33eb166288bdb329fb64ebda634
James Mead <james@floehopper.org>
2013-08-20 10:24:50 +0100

    "Extracting the Chat - Encapsulating the Chat", chapter 17.
    > From here, we can push everything to do with chat, its setup, and the use of
    > the `Announcer`, into `XMPPAuction`, adding management methods to the
    > `Auction` interface for its `AuctionEventListeners`. We're just showing the
    > end result here, but we changed the code incrementally so that nothing was
    > broken for more than a few minutes.
    >
    > Apart from the garbage collection "wart" this removes any references to
    > `Chat` from `Main`.

--- app/xmpp_auction.rb
+++ app/xmpp_auction.rb
@@ -1,9 +1,23 @@
+require "announcer"
+require "auction_message_translator"
+
 class XMPPAuction
   JOIN_COMMAND_FORMAT = "SQLVersion: 1.1; Command: JOIN;"
   BID_COMMAND_FORMAT = "SQLVersion: 1.1; Command: BID; Price: %d;"
 
-  def initialize(chat)
-    @chat = chat
+  AUCTION_RESOURCE = "Auction"
+  ITEM_ID_AS_LOGIN = "auction-%s"
+  AUCTION_ID_FORMAT = ITEM_ID_AS_LOGIN + "@%s/" + AUCTION_RESOURCE
+
+  def initialize(connection, item_id)
+    @auction_event_listeners = Announcer.new
+    @chat = connection.getChatManager.createChat(
+      auction_id(item_id, connection),
+      AuctionMessageTranslator.new(
+        connection.getUser,
+        @auction_event_listeners.announce
+      )
+    )
   end
 
   def bid(amount)
@@ -14,11 +28,19 @@ class XMPPAuction
     send_message(JOIN_COMMAND_FORMAT)
   end
 
+  def add_auction_event_listener(listener)
+    @auction_event_listeners.add_listener(listener)
+  end
+
   private
 
   def send_message(message)
     @chat.sendMessage(message)
   rescue XMPPException => e
     puts %{\n#{e}\n#{e.backtrace.join("\n")}}
+  end
+
+  def auction_id(item_id, connection)
+    format(AUCTION_ID_FORMAT, item_id, connection.getServiceName)
   end
 end
